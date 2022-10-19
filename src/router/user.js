81ef2725af063224d17b25fd4b007cdaca92a896
volibear <zhaop2014@sina.com>
2022-10-19 09:09:04 +0800

    feat: 设置cookie

--- src/router/user.js
+++ src/router/user.js
@@ -1,6 +1,13 @@
 const { loginFn } = require('../controller/user');
 const { SuccessModel, ErrorModel } = require('../model/responseModel');
 
+// 设置 cookie 过期时间
+const setCookieExpires = () => {
+  const d = new Date()
+  d.setTime(d.getTime() + 24 * 60 * 60 *1000)
+  return d.toGMTString()
+}
+
 const handleUserRouter = (req, res) => {
   const method = req.method;
 
@@ -10,10 +17,20 @@ const handleUserRouter = (req, res) => {
     const result = loginFn(username, password);
     return result.then(data => {
       if (data.username) {
+        // 操作 cookie
+        res.setHeader('Set-Cookie', `username=${data.username}; path=/; httpOnly; expires=${setCookieExpires()}`) // httpOnly 只允许服务端修改 cookie
         return new SuccessModel();
       }
       return new ErrorModel('登录失败');
     })
+  }
+
+  // 登录验证测试
+  if (method === 'GET' && req.path === '/api/user/login-test') {
+    if (req.cookie.username) {
+      return new SuccessModel()
+    }
+    return new ErrorModel('尚未登录')
   }
 }
 
