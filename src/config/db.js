889f05a760ea51370f04654a51f8bf6398c89961
volibear <zhaop2014@sina.com>
2022-10-23 16:58:31 +0800

    feat: 封装redis通用方法

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,10 @@
 const env = process.env.NODE_ENV
 
 let MYSQL_CONFIG
+let REDIS_CONFIG
 
 if (env === 'dev') {
+  // mysql
   MYSQL_CONFIG = {
     host: 'localhost',
     user: 'root',
@@ -10,17 +12,29 @@ if (env === 'dev') {
     port: 3306,
     database: 'blog_system'
   }
+  // redis
+  REDIS_CONFIG = {
+    host: '127.0.0.1',
+    port: 6379
+  }
 }
 if (env === 'production') {
+  // mysql
   MYSQL_CONFIG = {
     host: 'localhost',
     user: 'root',
     password: 'root123456',
     port: 3306,
     database: 'blog_system'
+  }
+  // redis
+  REDIS_CONFIG = {
+    host: '127.0.0.1',
+    port: 6379
   }
 }
 
 module.exports = {
-  MYSQL_CONFIG
+  MYSQL_CONFIG,
+  REDIS_CONFIG
 }
